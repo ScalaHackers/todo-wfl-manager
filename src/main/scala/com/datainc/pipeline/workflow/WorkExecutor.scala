7c3c631642a97a2e49742e302bf16fd7926eae71
Howard Dong <hdong@fulgent.com>
2016-03-03 11:36:26 -0800

    Updated WorkExecutor.

--- src/main/scala/com/datainc/pipeline/workflow/WorkExecutor.scala
+++ src/main/scala/com/datainc/pipeline/workflow/WorkExecutor.scala
@@ -1,14 +1,18 @@
 package com.datainc.pipeline.workflow
 
+import scala.sys.process._
 import akka.actor.Actor
 
 class WorkExecutor extends Actor {
 
   def receive = {
-    case n: Int =>
-            val n2 = n * n
-            val result = s"$n * $n = $n2"
+    case dataPrep: Int =>
+      val cmd = "ls -al"
+      val result = cmd.!!
+      //println(result)
       sender() ! TodoWorker.WorkComplete(result)
+    case _ =>
+      println("undefined!")
   }
 
 }
