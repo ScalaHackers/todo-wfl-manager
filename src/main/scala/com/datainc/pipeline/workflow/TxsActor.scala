c1be80e3975f25f3dcf1dbf0e3448f98333e31eb
Howard Dong <hdong@fulgent.com>
2016-03-03 11:01:30 -0800

    updated with some fixes.

--- src/main/scala/com/datainc/pipeline/workflow/TxsActor.scala
+++ src/main/scala/com/datainc/pipeline/workflow/TxsActor.scala
@@ -1,17 +1,12 @@
 package com.datainc.pipeline.workflow
 
-import akka.actor.ActorLogging
-import akka.persistence.PersistentActor
+import akka.actor.{Actor, ActorLogging}
 
 import scala.concurrent.duration.FiniteDuration
 
 /**
   * Created by hdong on 3/1/2016.
   */
-class TxsActor (workTimeout: FiniteDuration) extends PersistentActor with ActorLogging {
-  override def receiveRecover: Receive = ???
+abstract class TxsActor (workTimeout: FiniteDuration) extends Actor with ActorLogging {
 
-  override def receiveCommand: Receive = ???
-
-  override def persistenceId: String = ???
 }
--- src/main/scala/com/datainc/pipeline/workflow/TxsState.scala
+++ src/main/scala/com/datainc/pipeline/workflow/TxsState.scala
@@ -6,8 +6,8 @@ object TxsState {
 
   case class dataPrep(id: String) extends BasicState
 
-  case class dataValidate(id: String extends BasicState
+  case class dataValidate(id: String) extends BasicState
 
-  case class dataAnnotate(id: String extends BasicState
+  case class dataAnnotate(id: String) extends BasicState
 
 }
